fcd91e2c9cbbc24cbd9ebf1e0c0d7fdb027388f7
extg <extg@yandex-team.ru>
2020-10-22 20:31:14 +0300

    fetch data

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,78 @@
 import React from 'react';
-import logo from './react.svg';
-import './Home.css';
+import useSWR from 'swr'
+import axios from 'axios'
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div className="Home">
-        <div className="Home-header">
-          <img src={logo} className="Home-logo" alt="logo" />
-          <h2>Welcome to Razzle</h2>
-        </div>
-        <p className="Home-intro">
-          To get started, edit <code>src/App.js</code> or{' '}
-          <code>src/Home.js</code> and save to reload.
-        </p>
-        <ul className="Home-resources">
-          <li>
-            <a href="https://github.com/jaredpalmer/razzle">Docs</a>
-          </li>
-          <li>
-            <a href="https://github.com/jaredpalmer/razzle/issues">Issues</a>
-          </li>
-          <li>
-            <a href="https://palmer.chat">Community Slack</a>
-          </li>
-        </ul>
-      </div>
-    );
+import logo from './react.svg';
+import styles from './Home.module.css';
+import Toc from './Toc';
+
+const fetcher = (url) => axios.get(url).then(response => response.data)
+
+// const t = [
+//   {
+//     id,
+//     url,
+//     anchor,
+//     title,
+//     items: [
+//       {
+//         id,
+//         url,
+//         anchor,
+//         title,
+//         items: [
+
+//         ]
+//       }
+//     ]
+//   }
+// ]
+
+const createTree = ({ entities, id }) => {
+    const page = entities.pages[id]
+
+    return {
+      id: page.id,
+      url: page.url,
+      title: page.title,
+      pages: page.pages?.map(pageId => createTree({ entities, id: pageId })),
+      anchors: page.anchors?.map(anchorId => {
+        const anchor = entities.anchors[anchorId]
+
+        return {
+          id: anchor.id,
+          title: anchor.title,
+          url: anchor.url + anchor.anchor,
+        }
+      })
+    }
+}
+
+const Home = () => {
+  const { data } = useSWR('/HelpTOC.json', fetcher)
+
+  console.log('data', data)
+
+  if (!data) {
+    return null
   }
+
+  const tree = data.topLevelIds.map(id => createTree({ entities: data.entities, id }))
+
+  console.log('tree', tree)
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.toc}>
+        <nav>
+          <Toc tree={data} />
+        </nav>
+      </div>
+      <main>
+        hello
+      </main>
+    </div>
+  );
 }
 
 export default Home;
